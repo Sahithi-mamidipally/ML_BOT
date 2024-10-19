7b6535766b4f249257b438a2161366c85e589a3d
Sahithi Mamidipally <sahimamidi@gmail.com>
2024-10-19 12:12:53 -0500

    Add gemini-clone folder and update package files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Change 'your-repo-name' to the name of your GitHub repository if hosting in a subdirectory
 export default defineConfig({
+  base: '/ML_BOT/',  // Uncomment and replace if necessary
   plugins: [react()],
 })
